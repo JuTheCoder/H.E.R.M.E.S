b052efe9389e3055c1391840a74b8ece0a65ff99
JuTheCoder <gonzalezmd0417@gmail.com>
2026-03-05 18:14:50 -0500

    Created two endpoints for our api that can send data and threshold levels to our frontend.

--- frontend/test.js
+++ frontend/test.js
@@ -7,4 +7,45 @@ function myFunction(){
     document.getElementById("air_level").innerHTML = 20.0;
 }
 
+const api_data_url = 'http://127.0.0.1:8000/api/data';
+const api_thres_url = 'http://127.0.0.1:8000/api/threshold';
+
+// Fetching the data endpoint
+fetch(api_data_url)
+    .then(response =>{
+        if (!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse the JSON response
+    })
+    .then(data => {
+        console.log(data); // Handle the response data
+        // Ex: display data in an HTML element
+       // document.getElementById("test").innerHTML = data.message;
+        document.getElementById("temperature_level").innerHTML = data.temperature;
+        document.getElementById("co2_level").innerHTML = data.co2;
+        document.getElementById("co_level").innerHTML = data.co;
+        document.getElementById("air_level").innerHTML = data.air;
+    })
+    .catch(error => {
+        console.error('Problem with fetch operation: ', error);
+    });
+
+// fetching the threshold endpoint
+fetch(api_thres_url)
+    .then(response =>{
+        if (!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json()
+    })
+    .then(data => {
+        console.log(data);
+
+        document.getElementById("temp_threshold").innerHTML = data.temp_thresh;
+        document.getElementById("co2_threshold").innerHTML = data.co2_thresh;
+        document.getElementById("co_threshold").innerHTML = data.co_thresh;
+        document.getElementById("air_threshold").innerHTML = data.air_thresh;
+    })
+
 
