92ec3b8ce7c3b43c90eff964b3289471febb479c
Courtarj <courtarjeremy@gmail.com>
2026-03-05 23:42:53 -0500

    Updated sensors.ino
    Added logic for lcd and raspberry pi data retrieval, adjusted co sensor to use arduino pin A5

--- arduino_code/sensors/sensors.ino
+++ arduino_code/sensors/sensors.ino
@@ -1,43 +1,44 @@
-// Necessary libraries
 #include <SoftwareSerial.h>
 #include <MHZ19.h>
+#include <LiquidCrystal.h>
 
-// Sets arduino pin 10 as co2 receiver, pin 11 as co2 transmitter, and A1 as co reader
-#define CO2_RX 10
-#define CO2_TX 11
-#define MQ2_PIN A1
+#define CO2_RX 10 // Arduino listens to co2 sensor on pin 10
+#define CO2_TX 11 // Arduino talks to co2 sensor on pin 11 
+#define co_pin A5
 
+// Configures the LCD with its Control (12, 8) and Data (5, 4, 3, 2) pins
+LiquidCrystal screen(12, 8, 5, 4, 3, 2); 
 MHZ19 co2Sensor;
-// Sets up a separate communication channel for arduino and co2 sensor
+
+// Links the SoftwareSerial logic to co2 sensor pins
 SoftwareSerial co2Serial(CO2_RX, CO2_TX);
 
 void setup() {
-    // Uses standard baud rate of 9600 for communication 
     Serial.begin(9600);
-    co2Serial.begin(9600);
-    co2Sensor.begin(co2Serial);
+    co2Serial.begin(9600); // Starts the software communication at the sensor's required speed
+    co2Sensor.begin(co2Serial); // Tells the MHZ19 library which serial port to use
     
-    // Explicitly sets A1 as an input to read voltage
-    pinMode(MQ2_PIN, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    screen.begin(16, 2); // Indicates the screen has 16 columns and 2 rows
+    screen.clear();
+    
+    delay(2000); // Wait 2 seconds to allow sensor heaters to stabilize
 }
 
 void loop() {
-    static unsigned long timer = 0;
+    
+    // Gets co2 and co levels from respective sensors
+    int co2 = co2Sensor.getCO2(); 
+    int co = analogRead(co_pin);
 
-    // Checks if timer has reached 2 seconds
-    if (millis() - timer >= 2000) {
-        // Reads the co2 sensor level
-        int co2 = co2Sensor.getCO2();
-        
-        // Reads the co sensor level
-        int mq2 = analogRead(MQ2_PIN);
+    // Displays data on lcd
+    screen.setCursor(0, 0);
+    screen.print("CO2: "); screen.print(co2); screen.print("    ");
+    screen.setCursor(0, 1);
+    screen.print("CO: "); screen.print(co); screen.print("    ");
 
-        // Format for the Raspberry Pi/Serial Monitor output
-        Serial.print(co2);
-        Serial.print(",");
-        Serial.println(mq2);
+    // Sends data to the raspberry pi
+    Serial.print(co2); Serial.print(","); Serial.println(co);
 
-        // restarts timer
-        timer = millis();
-    }
+    delay(3000); // Waits 3 seconds before getting the next set of levels
 }
